fe273e2cb13e5b5059bdf00953c2688a4351bf43
David James Carter <fresco.raja@gmail.com>
2015-07-31 12:31:02 -0700

    add image uploader, limit file size

--- app/models/trip_plan.rb
+++ app/models/trip_plan.rb
@@ -1,2 +1,13 @@
 class TripPlan < ActiveRecord::Base
+  default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+  validates :title, presence: true
+  validate :picture_size
+
+  private
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
 end
